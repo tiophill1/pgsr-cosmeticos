e0153c90bbf5d40245ff8cbac4fd46fd2b0df01b
Gustavo Costa <tiophill993@gmail.com>
2023-08-25 09:40:21 -0300

    criação

---
+++ src/main/java/com/comesticos/tcc/pgsrcosmeticos/PgsrCosmeticosApplication.java
@@ -0,0 +1,13 @@
+package com.comesticos.tcc.pgsrcosmeticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgsrCosmeticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgsrCosmeticosApplication.class, args);
+	}
+
+}
