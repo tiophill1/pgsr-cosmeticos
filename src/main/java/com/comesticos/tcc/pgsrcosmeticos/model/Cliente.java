6073e83b8b0ef8db6a0e8845a0e8b8f1ecfbcfd2
Gustavo Costa <tiophill993@gmail.com>
2023-09-01 09:30:59 -0300

    camada de model concluída

--- src/main/java/com/comesticos/tcc/pgsrcosmeticos/model/Cliente.java
+++ src/main/java/com/comesticos/tcc/pgsrcosmeticos/model/Cliente.java
@@ -1,5 +1,71 @@
 package com.comesticos.tcc.pgsrcosmeticos.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="clientes")
 public class Cliente {
+	
+	@Id  // PK 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-Incremento de 1 em 1 
+	private Long id;
+	
+	private String nome;
+	private String email;
+	private String senha;
+	private String telefone;
+	private String cpf;
+	private boolean codStatus;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public boolean isCodStatus() {
+		return codStatus;
+	}
+	public void setCodStatus(boolean codStatus) {
+		this.codStatus = codStatus;
+	}
+	
+	
+	
 
 }
