0a18a0c986b350426145c4c90eff671f38231518
Gustavo Costa <tiophill993@gmail.com>
2023-10-25 09:55:29 -0300

    mensagem

--- src/main/java/com/comesticos/tcc/pgsrcosmeticos/controller/ClienteController.java
+++ src/main/java/com/comesticos/tcc/pgsrcosmeticos/controller/ClienteController.java
@@ -8,12 +8,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
- 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.comesticos.tcc.pgsrcosmeticos.model.Cliente;
 import com.comesticos.tcc.pgsrcosmeticos.repository.ClienteRepository;
@@ -29,7 +33,7 @@ public class ClienteController {
 	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/listar-cliente")
-	public String tabelaClientes(Model model, Cliente cliente) {
+	public String tabelaClientes(Cliente cliente, Model model) {
 		model.addAttribute("cliente", clienteRepository.findAll());
 		return "crud";
 	}
@@ -55,8 +59,48 @@ public class ClienteController {
 		Cliente clienteDB = clienteRepository.save(cliente);
 		cliente.setCodStatus(true);
 		
-		return "redirect:/pgsr/cliente/control";
+		return "redirect:/pgsr/cliente/listar-cliente";
 	}
 	
+	@GetMapping("/editar-prod/{id}")
+	public String showUpdateForm(@PathVariable("id") long id, ModelMap model) {
+		Cliente cliente = clienteRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid prod Id:" + id));
+	
+
+
+		model.addAttribute("cliente", cliente);
+		
+		return "Editar";
+	}
+
+	@PostMapping("/update/{id}")
+	public String atualizarProd(
+			@RequestParam(value = "file", required = false) MultipartFile file, @PathVariable("id") Long id, 
+			@ModelAttribute("prod") Cliente cliente, BindingResult result) {
+
+		if (result.hasErrors()) {
+			cliente.setId(id);
+			return "Editar";
+		}
+		
+
+		clienteRepository.save(cliente);
+		return "redirect:/pgsr/cliente/listar-cliente";
+	}
+	
+	@PostMapping
+	public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(Cliente cliente, @PathVariable("id") Long id) {
+		clienteRepository.deleteById(id);
+		return "redirect:/pgsr/cliente/listar-cliente";
+	}
     
 }
